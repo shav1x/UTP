76af297aebcce8e410216898fd501722f79a394b
shav1x <alexo27112005@gmail.com>
2024-10-11 15:58:11 +0200

    Initial project setup

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Adder adder = new Adder();
+        System.out.println(adder.add(1, 2));
+
+        Substractor substractor = new Substractor();
+        System.out.println(substractor.substract(6, 3));
+    }
+
+}
